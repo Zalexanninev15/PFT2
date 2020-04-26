e0d09c6136ca9e849bfb9efd9cffc73f1e41387f
Zalexanninev15 <blue.shark@disroot.org>
2020-04-26 14:32:00 +0300

    Add files via upload

---
+++ FDFmini/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("FDFmini")]
+[assembly: AssemblyDescription("Tool for work with FDF format of files")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Zalexanninev15 and Alexander927")]
+[assembly: AssemblyProduct("FDFmini")]
+[assembly: AssemblyCopyright("Copyright ©  2020, Zalexanninev15")]
+[assembly: AssemblyTrademark("FDFmini")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("57a4e5f4-c203-44d6-bcf9-27a771dbaeae")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]

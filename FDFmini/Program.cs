3319b6896babab40f55b742277c1e28b065a150b
Zalexanninev15 <blue.shark@disroot.org>
2020-07-07 14:49:44 +0300

    Final version [1.3.1]

--- FDFmini/Program.cs
+++ FDFmini/Program.cs
@@ -9,7 +9,7 @@ namespace FDFmini
 
         static void Main(string[] args)
         {
-            Console.Title = "FDFmini [Version 1.3]";
+            Console.Title = "FDFmini [Version 1.3.1]";
             if (args.Length != 0) 
             {
                 try
@@ -34,16 +34,16 @@ namespace FDFmini
                 Console.WriteLine("FDFmini [Argument 1] [path_to_file] [Argument 2] [Argument 3]");
                 Console.WriteLine();
                 Console.WriteLine("Argument 1:");
-                Console.WriteLine("-fdf (for FDF file)");
                 Console.WriteLine("-img (for IMG or BIN file)");
+                Console.WriteLine("-fdf (for FDF file)");
                 Console.WriteLine();
                 Console.WriteLine("Argument 2:");
-                Console.WriteLine("name of convertible file (for Argument 3 = -u)");
                 Console.WriteLine("name of new FDF file (for Argument 3 = -c)");
+                Console.WriteLine("name of convertible (new IMG) file (for Argument 3 = -u)");
                 Console.WriteLine();
                 Console.WriteLine("Argument 3:");
-                Console.WriteLine("-u (convert FDF to IMG, for Argument 1 = -fdf)");
                 Console.WriteLine("-c (convert IMG to FDF, for Argument 1 = -img)");
+                Console.WriteLine("-u (convert FDF to IMG, for Argument 1 = -fdf)");
                 Console.WriteLine();
                 Console.WriteLine("Examples:");
                 Console.WriteLine("FDFmini -img boot.img boot.fdf -c");
@@ -64,9 +64,9 @@ namespace FDFmini
                     if ((fileInf.Extension == ".img") || (fileInf.Extension == ".temp"))
                     {
                         Console.WriteLine("Converting IMG file to FDF...");
-                        using (FileStream sourceStream = new FileStream(sourceFile, FileMode.OpenOrCreate))
+                        using (FileStream sourceStream = new FileStream(@sourceFile, FileMode.OpenOrCreate))
                         {
-                            using (FileStream targetStream = File.Create(compressedFile))
+                            using (FileStream targetStream = File.Create(@compressedFile))
                             {
                                 using (GZipStream compressionStream = new GZipStream(targetStream, CompressionMode.Compress))
                                 {
@@ -100,9 +100,9 @@ namespace FDFmini
                     if (fileInf.Extension == ".fdf")
                     {
                         Console.WriteLine("Converting FDF file to IMG...");
-                        using (FileStream sourceStream = new FileStream(compressedFile, FileMode.OpenOrCreate))
+                        using (FileStream sourceStream = new FileStream(@compressedFile, FileMode.OpenOrCreate))
                         {
-                            using (FileStream targetStream = File.Create(targetFile))
+                            using (FileStream targetStream = File.Create(@targetFile))
                             {
                                 using (GZipStream decompressionStream = new GZipStream(sourceStream, CompressionMode.Decompress))
                                 {

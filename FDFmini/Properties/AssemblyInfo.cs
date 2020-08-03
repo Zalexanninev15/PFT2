595572251754cad759b6f59a9d5ae98b84332d74
Max Shabarchin <maks.shabarchin@yandex.ru>
2020-08-03 18:49:50 +0300

    Version 1,4

--- FDFmini/Properties/AssemblyInfo.cs
+++ FDFmini/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанные с этой сборкой.
 [assembly: AssemblyTitle("FDFmini")]
-[assembly: AssemblyDescription("Tool for work with FDF format of files")]
+[assembly: AssemblyDescription("Utility for work with FDF format of files")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Zalexanninev15 and Alexander927")]
+[assembly: AssemblyCompany("Zalexanninev15 & Alexander927")]
 [assembly: AssemblyProduct("FDFmini")]
-[assembly: AssemblyCopyright("Copyright ©  2020, Zalexanninev15")]
-[assembly: AssemblyTrademark("FDFmini")]
+[assembly: AssemblyCopyright("Copyright©  2020, Zalexanninev15")]
+[assembly: AssemblyTrademark("FDFmini (2020)")]
 [assembly: AssemblyCulture("")]
 
 // Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.1.0")]
-[assembly: AssemblyFileVersion("1.3.1.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]

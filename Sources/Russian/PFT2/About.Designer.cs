89695366881c7c0549fbf956c9eb2c08953299ec
Max Shabarchin <maks.shabarchin@yandex.ru>
2020-10-08 17:56:42 +0300

    Version 1.20.3.6
    Changes:
    - Added information when the app is first launched
    - Updated the app icon
    - Updated the description
    - Minor edits
    - Improvements for the Russian version of the app
    - Extra code removed
    
    Изменения:
    - Добавлена информацию при первом запуске приложения
    - Обновлена иконка приложения
    - Обновлено описание
    - Мелкие правки
    - Улучшения для русской версии приложения
    - Удалён лишний код

--- Sources/Russian/PFT2/About.Designer.cs
+++ Sources/Russian/PFT2/About.Designer.cs
@@ -64,7 +64,7 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = global::PFT2.Properties.Resources.PFT2;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(95, 68);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(104, 102);

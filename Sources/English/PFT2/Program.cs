e6d2c485af05b210433f924899fce304fccbf28d
Max Shabarchin <maks.shabarchin@yandex.ru>
2020-12-20 12:27:14 +0300

    Fix

--- Sources/English/PFT2/Program.cs
+++ Sources/English/PFT2/Program.cs
@@ -18,7 +18,7 @@ namespace PFT2
             }
             if (pftf != "1")
             {
-                DialogResult result = MessageBox.Show("By using this SOFTWARE, you agree that you are responsible for your further actions and their consequences. The author only supports the \"FT2\" project and answers questions that are directly related to the PTT 2 application itself. Of the devices, the author can only help with ZTE Blade V9 Vita!", "Agreement of use of the application", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                DialogResult result = MessageBox.Show("By using this SOFTWARE, you agree that you are responsible for your further actions and their consequences. The author only supports the \"PFT2\" project and answers questions that are directly related to the PFT2 application itself. Of the devices, the author can only help with ZTE Blade V9 Vita!", "Agreement of use of the application", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
                 if (result == DialogResult.Yes)
                 {
                     using (Microsoft.Win32.RegistryKey reg = Microsoft.Win32.Registry.CurrentUser.CreateSubKey(@"Software\Zalexanninev15\PFT2"))
